a5b28016e8d5cec893fad074bdfa7305e26eee25
sounkou-bioinfo <sounkoutoure@gmail.com>
2025-08-24 17:09:16 +0400

    path stuff

--- R/Clib.R
+++ R/Clib.R
@@ -1,8 +1,9 @@
-clib_binaries <- c(
+clib_commands <- c(
     "clib", "clib-install", "clib-search", "clib-init",
     "clib-configure", "clib-build", "clib-update",
     "clib-upgrade", "clib-uninstall"
 )
+clib_binaries <- clib_commands
 if (.Platform$OS.type == "windows") {
     clib_binaries <- paste0(clib_binaries, ".exe")
 }
@@ -27,11 +28,23 @@ get_clib_binary <- function(binary) {
 
 #' clib run
 #' Run a clib command
+#' trouble with interactive commands
+#' @param command Name of the clib command to run
 #' @param args Arguments to pass to clib
 #' @return Output of the clib command
 #' @export
-clib_run <- function(args) {
-    clib_path <- get_clib_binary("clib")
-    result <- system2(clib_path, args = args, stdout = TRUE, stderr = TRUE)
+clib_run <- function(command, args = character()) {
+    stopifnot(command %in% clib_commands)
+    clib_path <- get_clib_binary(command)
+    bin_dir <- dirname(clib_path)
+    # add it to PATH
+    OldPATH <- Sys.getenv("PATH")
+    on.exit(Sys.setenv(PATH = OldPATH), add = TRUE)
+    Sys.setenv(PATH = paste(bin_dir, OldPATH, sep = .Platform$path.sep))
+    PATH <- Sys.getenv("PATH")
+    result <- system2(clib_path,
+        args = args, stdout = TRUE, stderr = TRUE,
+        env = paste0("PATH=", PATH)
+    )
     return(result)
 }
